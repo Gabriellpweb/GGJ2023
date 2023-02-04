6d9da41df6c920eb81c69470ed25d546be6a6355
TCAL <thiagovccalheiros@gmail.com>
2023-02-04 13:13:55 +0000

    changes on damageable comp, not work 100%

--- Assets/Scripts/DamageableObject.cs
+++ Assets/Scripts/DamageableObject.cs
@@ -5,10 +5,51 @@ using UnityEngine;
 public class DamageableObject : MonoBehaviour
 {
     int lifePoints = 10;
+    float lastAttackTime;
+    float updateInterval = 0.5F;
+    int attackRate = 1;
+
+    protected GameObject target;
     public enum DamageableObjectTypes
     {
         Enemy,
         Player
     }
     public DamageableObjectTypes type;
+
+    public static string getPlayerTag()
+    {
+        return $"{DamageableObjectTypes.Player}";
+    }
+
+    void Attack()
+    {
+       
+        if (target == null) { //there is no target, nothing to do here
+            Debug.Log("Attack method target NULL");
+            return;
+        }
+        Debug.Log("Attack method w/ target");
+        float currentTime = Time.time;
+
+        if (lastAttackTime == null || (currentTime - lastAttackTime > attackRate)) {
+            lastAttackTime = currentTime;
+            DamageableObject damageableComp = target.GetComponent<DamageableObject>();
+            Debug.Log("VAI MORRE!! VAI MORRE!");
+        }
+    }
+
+    void IsItAlive()
+    {
+        if (lifePoints <= 0)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        IsItAlive();
+        Attack();
+    }
 }
--- Assets/Scripts/EnemyNavMeshObject.cs
+++ Assets/Scripts/EnemyNavMeshObject.cs
@@ -4,47 +4,49 @@ using UnityEngine;
 using UnityEngine.AI;
 public class EnemyNavMeshObject : MonoBehaviour
 {
-    public NavMeshAgent navMeshAgent;
-    //[SerializeField] private Transform movePositionTransform;
-    private void Awake()
-    {
-        navMeshAgent = GetComponent<NavMeshAgent>();
-    }
+    //public NavMeshAgent navMeshAgent;
+    ////[SerializeField] private Transform movePositionTransform;
+    //private void Awake()
+    //{
+    //    navMeshAgent = GetComponent<NavMeshAgent>();
+    //}
 
 
-    void FindClosestPlayer()
-    {
-        NavMeshPath path = new NavMeshPath();
-        GameObject[] players = GameObject.FindGameObjectsWithTag($"{DamageableObject.DamageableObjectTypes.Player}");
-        float ShortestDistance = float.MaxValue;
+    //public void FindClosestPlayer()
+    //{
+    //    NavMeshPath path = new NavMeshPath();
+    //    GameObject[] players = GameObject.FindGameObjectsWithTag(DamageableObject.getPlayerTag());
+    //    float ShortestDistance = float.MaxValue;
 
-        Transform target = null;
+    //    Transform target = null;
 
-        foreach (GameObject player in players) {
-            if (!NavMesh.CalculatePath(transform.position, player.transform.position, navMeshAgent.areaMask, path)) { //err during calc
-                continue;
-            }
+    //    foreach (GameObject player in players) {
+    //        if (!NavMesh.CalculatePath(transform.position, player.transform.position, navMeshAgent.areaMask, path)) { //err during calc
+    //            continue;
+    //        }
 
-            float distance = Vector3.Distance(transform.position, path.corners[0]);
-            for (int i = 1; i < path.corners.Length; i++) {
-                distance += Vector3.Distance(path.corners[i-1], path.corners[i]);
-            }
+    //        float distance = Vector3.Distance(transform.position, path.corners[0]);
+    //        for (int i = 1; i < path.corners.Length; i++) {
+    //            distance += Vector3.Distance(path.corners[i-1], path.corners[i]);
+    //        }
 
-            if (distance < ShortestDistance) {
-                target = player.transform;
-                ShortestDistance = distance;
-            } 
+    //        if (distance < ShortestDistance) {
+    //            target = player.transform;
+    //            ShortestDistance = distance;
+    //        } 
             
-        }
+    //    }
 
-        if (target != null) {
-            navMeshAgent.destination = target.position;
-        }
-    }
+    //    if (target != null) {
+    //        navMeshAgent.destination = target.position;
+    //    }
+
+    //    Debug.Log("Eu fui executado ");
+    //}
 
     private void Start()
     {
-        FindClosestPlayer();
+        //FindClosestPlayer();    
     }
 
     private void Update()
